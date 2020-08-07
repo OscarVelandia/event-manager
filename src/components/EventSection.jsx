a9bc43e1562a04dbd7ad13e33f3a16d5ed0434c5
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2020-08-07 01:05:01 -0300

    * Use getEvents and getSections services * Implements EventSection component

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from './Card';
+import { getEvents } from '../utils/api/services';
 
-export default function EventSection() {
-  return <div />;
+export default function EventSection({ title, limit }) {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    return () => {
+      getEvents().then(setEvents);
+    };
+  }, []);
+
+  const eventCard = () => {
+    if (limit) setEvents(events.slice(0, limit));
+
+    return events.map(({ id, label, description, location, date, categoryId }) => (
+      <Card key={id} {...{ label, description, location, date, categoryId }} />
+    ));
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      {eventCard}
+    </div>
+  );
 }
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Button from '../components/Button';
+import EventSection from '../components/EventSection';
+import { getCategories } from '../utils/api/services';
+import generateId from '../utils/generateId';
+
+const MAX_UPCOMING_EVENTS = 5;
+const DEFAULT_SECTION = {
+  id: generateId(),
+  label: `Upcoming ${MAX_UPCOMING_EVENTS} events`,
+  limit: MAX_UPCOMING_EVENTS,
+};
 
 function App() {
+  const [sections, setSections] = useState([DEFAULT_SECTION]);
+
+  useEffect(() => {
+    getCategories().then((categories) => setSections(sections.concat(categories)));
+    console.log(sections);
+  }, [sections]);
+
   const handleCreateEventClick = (event) => {
     event.preventDefault();
     console.log(event);
   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button
-          onClick={handleCreateEventClick}
-          text="Create Event"
-          type="open"
-          icon="plus"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <main className="App">
+      <Button
+        onClick={handleCreateEventClick}
+        text="Create Event"
+        type="open"
+        icon="add"
+      />
+
+      {sections.map(({ id, label, limit }) => (
+        <EventSection key={id} title={label} limit={limit} />
+      ))}
+    </main>
   );
 }
 
