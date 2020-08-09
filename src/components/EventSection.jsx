2c8d5780712a1e457bfa2d64fa2271649a45fb82
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2020-08-08 22:54:17 -0300

    Move fetch logic to the App page

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Card from './Card';
-import { getEvents } from '../utils/api/services';
 
-export default function EventSection({ title, limit }) {
-  const [events, setEvents] = useState([]);
-  useEffect(() => {
-    return () => {
-      getEvents().then(setEvents);
-    };
-  }, []);
-
-  const eventCard = () => {
-    if (limit) setEvents(events.slice(0, limit));
-
-    return events.map(({ id, label, description, location, date, categoryId }) => (
-      <Card key={id} {...{ label, description, location, date, categoryId }} />
-    ));
-  };
+export default function EventSection({ label, events = [] }) {
+  const eventCard = events.map(
+    ({ id, _label, description, location, date, categoryId }) => (
+      <Card key={id} {...{ _label, description, location, date, categoryId }} />
+    ),
+  );
 
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>{label}</h2>
       {eventCard}
     </div>
   );
