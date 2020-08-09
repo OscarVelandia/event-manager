29ac606d6e5f098a686b53bd442f0242cfbd3633
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2020-08-08 22:52:01 -0300

    Add Card templateY and css file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import styles from './Card.module.css';
 
-export default function Card() {
-  return <div />;
+export default function Card({ label, description, location, date, categoryId }) {
+  return (
+    <div>
+      <h3>{label}</h3>
+      <li>{description}</li>
+      <li>{location}</li>
+      <div className={styles.footer}>
+        <li>{date}</li>
+        <label htmlFor={categoryId}>
+          <input type="checkbox" id={categoryId} name="categoryId" />
+        </label>
+      </div>
+    </div>
+  );
 }
