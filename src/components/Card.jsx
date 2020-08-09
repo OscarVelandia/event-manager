abb30a86ad6c18ad7725ae682011fe863bdfec72
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2020-08-09 18:38:21 -0300

    * Add scale to the Card checkbox * Set card width with flex-basis * Fix styles to let add large description without broke the layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,15 @@ import styles from './Card.module.css';
 export default function Card({ eventLabel, description, location, date, categoryId }) {
   return (
     <div className={styles.cardContainer}>
-      <h3>{eventLabel}</h3>
-      <li>{description}</li>
-      <li>{location}</li>
+      <h3 className={styles.title}>{eventLabel}</h3>
+      <p className={styles.description}>
+        <span>{description}</span>
+      </p>
+      <p>{location}</p>
       <div className={styles.footer}>
-        <li>{date}</li>
-        <label htmlFor={categoryId}>
-          <input
-            type="checkbox"
-            id={categoryId}
-            name="categoryId"
-            className={styles.checkbox}
-          />
+        <p>{date}</p>
+        <label className={styles.checkbox} htmlFor={categoryId}>
+          <input type="checkbox" id={categoryId} name="categoryId" />
         </label>
       </div>
     </div>
