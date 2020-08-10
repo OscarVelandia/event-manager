44c73f41833213f503d50481488987cf3e2c2d4d
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2020-08-10 02:09:18 -0300

    Create generic Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,53 @@
 import React from 'react';
+import styles from './Input.module.css';
 
-export default function Input() {
-  return <div />;
+const INPUT_TYPE = {
+  checkbox: 'checkbox',
+  select: 'select',
+  input: 'input',
+};
+
+export default function Input({
+  type,
+  id,
+  options,
+  hasLabel,
+  customStyles,
+  onChange,
+  isSelected,
+}) {
+  const inputType = INPUT_TYPE[type];
+  const [optionsName] = options ? Object.keys(options) : [];
+  const optionsEl =
+    options &&
+    options.map(({ id: selectId, label }) => {
+      return (
+        <React.Fragment key={selectId}>
+          <option key={selectId} value={label}>
+            {label}
+          </option>
+        </React.Fragment>
+      );
+    });
+
+  return (
+    <div style={customStyles} className={`${styles[inputType]} ${styles.positioning}`}>
+      <label htmlFor={id} className={styles.title}>
+        {hasLabel && id}
+      </label>
+      {options ? (
+        <select onChange={({ target }) => onChange(target.value, id)} id={optionsName}>
+          {optionsEl}
+        </select>
+      ) : (
+        <input
+          onChange={({ target }) => onChange(target, id)}
+          type={type}
+          id={id}
+          name={id}
+          checked={isSelected}
+        />
+      )}
+    </div>
+  );
 }
