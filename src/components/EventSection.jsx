1eba43f96b308097bc54199d4b9bdb6f3d7fb321
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2020-08-10 02:08:36 -0300

    * Pass onChange prop * Rename id and label by less generic names * Modify style

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -2,14 +2,32 @@ import React from 'react';
 import Card from './Card';
 import styles from './EventSection.module.css';
 
-export default function EventSection({ label, events = [] }) {
+export default function EventSection({ label, events = [], onChange }) {
   return (
     <section>
       <h2 className={styles.sectionTitle}>{label}</h2>
       <div className={styles.cardContainer}>
         {events.map(
-          ({ id, label: eventLabel, description, location, date, categoryId }) => (
-            <Card key={id} {...{ eventLabel, description, location, date, categoryId }} />
+          ({
+            id: eventId,
+            label: eventLabel,
+            hasSubscription,
+            description,
+            location,
+            date,
+          }) => (
+            <Card
+              key={eventId}
+              {...{
+                eventLabel,
+                hasSubscription,
+                description,
+                location,
+                date,
+                eventId,
+                onChange,
+              }}
+            />
           ),
         )}
       </div>
